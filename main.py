12cc03678dc22f0687e350a9011c0764a064f218
zb46392 <zb46392@oss.unist.hr>
2019-01-20 17:34:01 +0100

    find next paths

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ BOARD_1 = {"n": 3, "m": 3, "x": ["bwe","wbw","ewb"]}
 BOARD_2 = {"n": 4, "m": 4, "x": ["beww","beww","beww","wewe"]}
 BOARD_3 = {"n": 20, "m": 20, "x": ["bwbwbwbwbwbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwb","bwbwbwbwbwbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwb","bwbwbwbwbwbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwb","bwbwbwbwewbwbwbwbwbw","wbwbwbwbebwbwbwbwbwb","bwbwbwbwewbwbwbwbwbw","wbwbwbwbebwbwbwbwbwb","bwbwbwbwewbwbwbwbwbw","wbwbwbwbebwbwbwbwbwb","bwbwbwbwewbwbwbwbwbw","wbwbwbwbebwbwbwbwbwb","bwbwbwbwewbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwb","bwbwbwbwbwbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwb","bwbwbwbwbwbwbwbwbwbw","wbwbwbwbwbwbwbwbwbwe"]}
 BOARD_4 = {"n": 2, "m": 8, "x": ["ewewbbbb","bwebewww"]}
+BOARD_5 = {"n": 12, "m": 6, "x": ["bewbbe", "eebwbw", "eeebwe", "bbeeww", "eebbee", "bbwbww", "wwbwww", "eeewwe", "ebwebe", "eewewe", "eweeeb", "eeebwe"]}
+BOARD_6 = {"n": 9, "m": 6, "x": ["ewbbww", "bbbbee", "weeeee", "bbeebe", "eewbwe", "weebee", "ewbeww", "wwwwbe", "bbewee"]}
+BOARD_7 = {"n": 2, "m": 6, "x": ["wbebbb", "ebbebe"]}
+BOARD_8 = {"n":12, "m":11, "x": ["eebbeebbwwb", "ewwwebbbwww", "bbbewebwbbb", "ebewweebeee", "ewbwbbewbbw", "wbeeebwbeee", "weewewewbbw", "ebwbbbeeeew", "webwebeewwb", "bbwweebbebe", "eebbbwebwbw", "beebbebwbbb"]}
+BOARD_9 = {"n": 2, "m": 15, "x": ["ebbeeeeweeebbeb", "eewwbbbbbebbbwb"]}
+BOARD_10 = {"n":16, "m": 16, "x": ["wwbeeewewwebwwwe", "bwbebebbwweweebe", "wwwbeebeebewbeew", "beeeeebwweweewbe", "wwwbebeweewbeeee", "webebbebeebewbbb", "bwewbwewbwbewebe", "ebbbbewbwewebbwb", "bweebwwebbewebwb", "ebewewebbwwbeebb", "wbebeewwebwbebwe", "eeewbewbweebebwb", "bwweebebbwbeebew", "bebbewbebbwwweew", "ebewbbwbwbeewbwb", "ewebbbwebewwbbbb"]}
+BOARD_11 = {"n":14, "m": 17, "x": ["ebbebebewebbwwbwe", "wbeewbwwebewbebeb", "weweeewwwwewbwweb", "wbewbbbwbeeeewewe", "bbewwebwbeebbbbew", "webeeeeweeeebeewb", "eebwbwbbbewbbeebe", "webeeebeebewbbwee", "wbebwbeewwbwebbbb", "eebwwewwwwbwweewb", "ebweeebeewewbbbwb", "wwebweeweebwbbebw", "eeebwbwewweewbebb", "wewweewwbbweebewe"]}
+BOARD_12 = {"n": 14, "m":17, "x":["ebbebebewebbwwbwe", "wbeewbwwebewbebeb", "weweeewwwwewbwweb", "wbewbbbwbeeeewewe", "bbewwebwbeebbbbew", "webeeeeweeeebeewb", "eebwbwbbbewbbeebe", "webeeebeebewbbwee", "wbebwbeewwbwebbbb", "eebwwewwwwbwweewb", "ebweeebeewewbbbwb", "wwebweeweebwbbebw", "eeebwbwewweewbebb", "wewweewwbbweebewe"]}
+BOARD_X = {"n":20,"m":20,"x":["bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbweewbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw","wwwwbbbbwwwwbbbbwwbb","bwbwbwbwbwbwbwbwbwbw", "wwwwbbbbwwwwbbbbwwbb"]}
 
 '''
 0 -> 3
@@ -19,9 +28,18 @@ BOARD_4 = {"n": 2, "m": 8, "x": ["ewewbbbb","bwebewww"]}
 2 -> 2
 3 -> 17
 4 -> 3
+5 -> 3 ! 5
+6 -> 3
+7 -> 2
+8 -> 3
+9 -> 2
+10 -> 3
+11 -> 3
+12 -> 2 ? 3
+X -> 6 ! 5
 '''
 
-BOARD = BOARD_4
+BOARD = BOARD_12
 
 
 def findMostPromisingPaths(path):
@@ -48,15 +66,30 @@ def findMostPromisingPaths(path):
             newPath.append(nextState["moves"])
             
             mostPromisingPaths.append({"board": nextState["board"], "moves": newPath})
-        '''
+        '''            
             
     return {"solved": False, "paths": mostPromisingPaths}
     
-
+def findNextPaths(path):
+    paths = []
+    
+    for nextState in path["board"].getNextStates():
+        if nextState["board"].isSolved():
+            path["moves"].append(nextState["moves"])
+            return {"solved" : True, "paths" : path["moves"]}
+            
+        newPath = [move for move in path["moves"]]
+        newPath.append(nextState["moves"])
+        
+        paths.append({"board": nextState["board"], "moves": newPath})
+        
+    return {"solved": False, "paths": paths}
+    
 def minimumMoves(n, m, x):
     board = Board(n, m, x)
     
-    paths = findMostPromisingPaths({"board": board, "moves": []})
+    #paths = findMostPromisingPaths({"board": board, "moves": []})
+    paths = findNextPaths({"board": board, "moves": []})
     
     if paths["solved"]:
         return paths["paths"]     
@@ -68,6 +101,7 @@ def minimumMoves(n, m, x):
         path = paths.pop(0) # PATH = ( BOARD , [ MOVES ])
         
         mostPromisingPaths = findMostPromisingPaths(path)
+        #mostPromisingPaths = findNextPaths(path)
         
         if mostPromisingPaths["solved"]:
             return mostPromisingPaths["paths"]
